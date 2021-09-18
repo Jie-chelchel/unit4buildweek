15be65ef2777a21b77fdf824c136602befe2f884
Jie Zhang <zhangjiew@gmail.com>
2021-09-17 18:58:46 -0600

    test

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 /*
 
   PORT=5000
@@ -15,22 +15,27 @@ require('dotenv').config()
     - testing_database_name (use the real name of the testing database you created in pgAdmin 4)
 
 */
-const pg = require('pg')
+const pg = require("pg");
 
 if (process.env.DATABASE_URL) {
-  pg.defaults.ssl = { rejectUnauthorized: false }
+  pg.defaults.ssl = { rejectUnauthorized: false };
 }
 
 const sharedConfig = {
-  client: 'pg',
-  migrations: { directory: './api/data/migrations' },
-  seeds: { directory: './api/data/seeds' },
-}
+  client: "pg",
+  migrations: { directory: "./data/migrations" },
+  seeds: { directory: "./data/seeds" },
+};
 
 module.exports = {
   development: {
     ...sharedConfig,
     connection: process.env.DEV_DATABASE_URL,
+    // pool: {
+    //   afterCreate: (conn, done) => {
+    //     conn.run("PRAGMA foreign_keys = ON", done);
+    //   },
+    // },
   },
   testing: {
     ...sharedConfig,
@@ -41,4 +46,4 @@ module.exports = {
     connection: process.env.DATABASE_URL,
     pool: { min: 2, max: 10 },
   },
-}
+};
