b7665d634d1efc0599b7d1d999197227f9d81086
Jie Zhang <zhangjiew@gmail.com>
2021-09-17 19:18:15 -0600

    fixed a typo

--- api/server.js
+++ api/server.js
@@ -8,6 +8,9 @@ const authRouter = require("./auth/auth-router");
 
 const usersRouter = require("./users/users-router");
 server.use(express.json());
+server.use("/", (req, res) => {
+  res.send("Hello");
+});
 server.use("/api/users", usersRouter);
 server.use("/api/auth", authRouter);
 
