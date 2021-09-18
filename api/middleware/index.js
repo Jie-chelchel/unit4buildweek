3865a0aec8926a6e3c3d418acd23f71619f1d39a
Jie Zhang <zhangjiew@gmail.com>
2021-09-17 23:49:09 -0600

    fixed registration bug

--- api/middleware/index.js
+++ api/middleware/index.js
@@ -9,7 +9,7 @@ const validateInput = (req, res, next) => {
   }
 };
 const userEmailExist = async (req, res, next) => {
-  const allUsers = await Users.find();
+  const allUsers = await Users.getAllUsers();
   const userExist = allUsers.find((u) => u.user_email === req.body.user_email);
   if (userExist) {
     res.status(400).json({ message: "this email has already been registered" });
