4a869c1c2f0e1270bd6708ec3492732caaafbe4d
Jie Zhang <zhangjiew@gmail.com>
2021-09-18 19:30:46 -0600

    test

--- data/migrations/20210124181032_first-migration.js
+++ data/migrations/20210124181032_first-migration.js
@@ -25,5 +25,5 @@ exports.up = function (knex) {
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("users").dropTableIfExists("items");
+  return knex.schema.dropTableIfExists("items").dropTableIfExists("users");
 };
